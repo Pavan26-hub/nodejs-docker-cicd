283f55b8083e1c73ee2c3e0592b53486738cbede
Pavan26-hub <raktate2001@gmail.com>
2026-02-05 20:03:26 +0000

    Update app message---1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Version 2 deployed via Jenkins CI/CD');
+  res.send('Version 2 deployed via Jenkins CI/CD----1');
 });
 
 /**
